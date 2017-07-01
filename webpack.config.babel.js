c10d43b98983b90eeac25a3cec149524d815ab81
liangze <249834096@qq.com>
2017-07-01 18:50:48 +0800

    es6

---
+++ webpack.config.babel.js
@@ -0,0 +1,17 @@
+import path from 'path'
+
+export default {
+  context: path.resolve(__dirname, 'js'),
+  entry: './main',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.html$/, loaders: ['raw'], exclude: /node_modules/},
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/},
+    ],
+  },
+}
+
